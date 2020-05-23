6b9c18318fe4402a3a38a477c1aa0eaf561d60dd
EddieUFSM <edrocha@inf.ufsm.br>
2020-05-23 12:15:53 -0300

    routes config init, mdb connection

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express();
 
 require('dotenv').config()
 
-app.get('/', (req, res) => {
-    res.send("Hello World!")
-})
+//import routes
+const userRoutes = require('./routes/user')
+
+/** Connect to DB */
+mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useUnifiedTopology: true}).then(()=> console.log("DB Connected"));
+//mongoose.connect(process.env.MDB_KEY);
+
+//routes middleware
+app.use("/api", userRoutes)
 
 const port = process.env.PORT || 8000
 
